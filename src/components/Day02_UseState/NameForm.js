1be933b3c9a17d26de0993bbd0b3f93563102ec5
mariaashwini <mariaashwini06@gmail.com>
2025-06-21 07:37:42 -0700

    Added header in NameForm.js file

--- src/components/Day02_UseState/NameForm.js
+++ src/components/Day02_UseState/NameForm.js
@@ -5,6 +5,7 @@ export default function NameForm()
     const [user,setUser] = useState('');
     return (
         <>
+        <h2>Controlled Form Input</h2>
         <input type="text" id="name" value={user} onChange={(e) => setUser(e.target.value)}/>
         <h1>Hello, {user || 'stranger'}</h1>
         </>
