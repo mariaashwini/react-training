d06237b696127d8442d3101f019842fcafa4dfb6
mariaashwini <mariaashwini06@gmail.com>
2025-06-21 02:21:46 -0700

    Adding Child Components <Counter/>,<NameForm/> and <Tabs/>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Welcome from './Welcome.js';
+import Counter from './Counter.js';
+import NameForm from './NameForm.js';
+import Tabs from './Tabs.js';
+
 
 function App() {
   return (
     <div className="App">
       <Welcome name="Maria"/>
       <Welcome name="Ashwini"/>
-
+      <Counter/>
+      <NameForm/> 
+      <Tabs/>
     </div>
   );
 }
