5dd84288df80335c22b73b5e600f1ae553ef4d43
mariaashwini <mariaashwini06@gmail.com>
2025-07-01 21:54:58 +0530

    Refactor: Enhanced Navbar component and applied css styles

--- src/components/Day05_Blog/Navbar.js
+++ src/components/Day05_Blog/Navbar.js
@@ -1,10 +1,28 @@
 import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/categories">Categories</NavLink>
+    <nav className="navbar-blog">
+      <div className="logo">My logo</div>
+      <div className="nav-links">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/categories"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Categories
+        </NavLink>
+      </div>
     </nav>
   );
 }
