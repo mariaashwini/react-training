b4939fcc7c791f45354085be34b6191394abfbd6
mariaashwini <mariaashwini06@gmail.com>
2025-07-01 21:46:55 +0530

    Refactor: Enhanced Welcome component UI and added emoji

--- src/components/Day01_Props/Welcome.js
+++ src/components/Day01_Props/Welcome.js
@@ -1,3 +1,9 @@
-export default function Welcome(props){
-    return <h3>Welcome {props.name}</h3>
+import './Welcome.css'
+export default function Welcome({name="Guest"}){
+     return (
+    <div className="welcome-box">
+      <h3>Welcome, {name} 🙋‍♀️</h3>
+      <p>Thanks for visiting! Hope you're having a great day.</p>
+    </div>
+  );
 }
